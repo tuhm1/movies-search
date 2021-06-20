e8361deb1b2551286a964fd74161d53c854dbcd3
tuhm1 <hmtu117@gmail.com>
2021-06-20 16:35:06 +0700

    Use config file

--- src/elasticsearch/index.js
+++ src/elasticsearch/index.js
@@ -1,7 +1,7 @@
 const { Client } = require("@elastic/elasticsearch");
 const util = require('util');
 
-const client = new Client({ node: 'http://localhost:9200' });
+const client = new Client(require('./config.json'));
 client.on('request', (err, result) => {
     console.log('request', util.inspect(JSON.parse(result.meta.request.params.body), false, null, true));
 });
